47b8bafed808a36fa260c1d00fa9b04b9afad686
supprogramer <agito11752@gmail.com>
2020-04-27 23:49:50 -0500

    cambio cinco

--- js/estilo.js
+++ js/estilo.js
@@ -1,8 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   M.AutoInit();
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems,options);
+  var instances = M.FloatingActionButton.init(elems, options);
 });
+
+
+function MM_showHideLayers() { //v9.0
+  var i, p, v, obj, args = MM_showHideLayers.arguments;
+  for (i = 0; i < (args.length - 2); i += 3)
+    with(document) if (getElementById && ((obj = getElementById(args[i])) != null)) {
+      v = args[i + 2];
+      if (obj.style) {
+        obj = obj.style;
+        v = (v == 'show') ? 'visible' : (v == 'hide') ? 'hidden' : v;
+      }
+      obj.visibility = v;
+    }
+}
